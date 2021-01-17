96fcdc67889aeaf1339057516cc4d84c61b0e398
robertdabrowski <69250878+robertdabrowski@users.noreply.github.com>
2021-01-17 12:32:53 +0100

    Update dev_inventory.py

--- dev_inventory.py
+++ dev_inventory.py
@@ -4,7 +4,7 @@ from ftntlib import FortiManagerJSON
 
 ip = '10.20.30.31'
 login = 'admin'
-password = 'fortigard'
+password = 'xxxxxxxxx'
 
 url='sys/proxy/json'
 resource="/api/v2/monitor/user/detected-device/select?&vdom=root&expand_child_macs=true&with_fortilink=true&with_fortiap=true&with_endpoint=true&with_dhcp=true&width_user=true"
