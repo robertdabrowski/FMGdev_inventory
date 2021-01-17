7463a0f7b02aa1964f85cba51954f5bcc035bfe6
robertdabrowski <69250878+robertdabrowski@users.noreply.github.com>
2021-01-17 12:31:42 +0100

    Add files via upload

---
+++ dev_inventory.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3.6
+
+from ftntlib import FortiManagerJSON
+
+ip = '10.20.30.31'
+login = 'admin'
+password = 'fortigard'
+
+url='sys/proxy/json'
+resource="/api/v2/monitor/user/detected-device/select?&vdom=root&expand_child_macs=true&with_fortilink=true&with_fortiap=true&with_endpoint=true&with_dhcp=true&width_user=true"
+
+data= {'target': ["adom/root/device/OT60FR"], 'action': 'get','resource':resource }
+
+
+api = FortiManagerJSON()
+api.login(ip, login, password)
+api.debug('on')
+status,mydata = api.execute(url,data)
+print(mydata[0]['response']['results'])
+api.debug('off')
+api.logout()
+
